244987d28cb9b8db02565d4c6672a21c1ce604e8
choi-ys <project.log.062@gmail.com>
2022-01-07 01:03:06 +0900

    [#10]Custom Request/Response Logging Filter 구현
    - AbstractGatewayFilterFactory의 GatewayFilter를 이용한 요청/응답의 전후 로깅
    - application.yml의 Filter 적용 대상 micro-service 설정부에 구현한 CustomFilter 등록

---
+++ gateway/src/main/java/io/ecommerce/gateway/filter/DefaultHttpLoggingFilter.java
@@ -0,0 +1,41 @@
+package io.ecommerce.gateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author : choi-ys
+ * @date : 2022-01-07 오전 12:48
+ */
+@Component
+@Slf4j
+public class DefaultHttpLoggingFilter extends AbstractGatewayFilterFactory<DefaultHttpLoggingFilter.Config> {
+
+    public DefaultHttpLoggingFilter() {
+        super(Config.class);
+    }
+
+    public static class Config {
+
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+
+            log.info("[CustomFilter][PRE] Response Code -> {}", request.getId());
+
+            return chain.filter(exchange)
+                    .then(Mono.fromRunnable(() -> {
+                        log.info("[CustomFilter][POST] Response Code -> {}", response.getStatusCode());
+                    }));
+        };
+    }
+}

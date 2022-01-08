9a192fcb8938bd5cff05db909de8a9cd6a45e147
choi-ys <project.log.062@gmail.com>
2022-01-08 17:08:55 +0900

    [#33][회원 검색 API]회원 검색 쿼리 Interface 정의

---
+++ user-service/src/main/java/io/ecommerce/userservice/core/domain/dto/request/SearchUserRequest.java
@@ -0,0 +1,46 @@
+package io.ecommerce.userservice.core.domain.dto.request;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author : choi-ys
+ * @date : 2022/01/08 3:39 오후
+ */
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class SearchUserRequest {
+    private String email;
+    private String name;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private Pageable pageable;
+
+    private SearchUserRequest(
+            final String email,
+            final String name,
+            final LocalDateTime createdAt,
+            final LocalDateTime updatedAt,
+            final Pageable pageable
+    ) {
+        this.email = email;
+        this.name = name;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.pageable = pageable;
+    }
+
+    public static SearchUserRequest of(
+            final String email,
+            final String name,
+            final LocalDateTime createdAt,
+            final LocalDateTime updatedAt,
+            final Pageable pageable
+    ) {
+        return new SearchUserRequest(email, name, createdAt, updatedAt, pageable);
+    }
+}
---
+++ user-service/src/main/java/io/ecommerce/userservice/core/repository/UserQueryRepo.java
@@ -0,0 +1,13 @@
+package io.ecommerce.userservice.core.repository;
+
+import io.ecommerce.userservice.core.domain.dto.request.SearchUserRequest;
+import io.ecommerce.userservice.core.domain.dto.response.UserResponse;
+import org.springframework.data.domain.Page;
+
+/**
+ * @author : choi-ys
+ * @date : 2022/01/08 3:38 오후
+ */
+public interface UserQueryRepo {
+    Page<UserResponse> searchUserPageBySearchParams(SearchUserRequest searchUserRequest);
+}

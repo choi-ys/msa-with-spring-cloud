b899bf2511bc9fb9f21a1df7f3abebf28ab66357
choi-ys <project.log.062@gmail.com>
2022-01-09 18:58:46 +0900

    [#25][상품조회 API]상품 조회 관련 API 및 TC 작성
     - 특정 상품 및 상품 목록 조회 API 및 TC 작성
     - 첫페이지 요청 시 0번 페이지를 1번 페이지로 처리할 Utils 작성
     - API TC 작성에 공통적으로 사용되는 설정 Custom Annotation으로 구성

---
+++ product-service/src/main/kotlin/io/ecommerce/productgservice/api/ProductController.kt
@@ -0,0 +1,41 @@
+package io.ecommerce.productgservice.api
+
+import io.ecommerce.productgservice.common.utils.PageUtils
+import io.ecommerce.productgservice.core.domain.Product
+import io.ecommerce.productgservice.core.domain.common.PageResponse
+import io.ecommerce.productgservice.core.domain.dto.response.ProductResponse
+import io.ecommerce.productgservice.core.service.ProductQueryService
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
+import org.springframework.data.web.PageableDefault
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * @author : choi-ys
+ * @date : 2022/01/09 6:04 오후
+ */
+@RestController
+@RequestMapping("product")
+class ProductController(
+    private val productQueryService: ProductQueryService,
+) {
+
+    @GetMapping("{id}")
+    fun findById(@PathVariable("id") id: Long): ResponseEntity<ProductResponse> =
+        ResponseEntity.ok(productQueryService.findById(id))
+
+    @GetMapping
+    fun findAllByPageRequest(
+        @PageableDefault(
+            page = 0,
+            size = 30,
+            sort = ["id"],
+            direction = Sort.Direction.DESC
+        ) pageable: Pageable,
+    ): ResponseEntity<PageResponse<Product>> =
+        ResponseEntity.ok(productQueryService.findAllByPageRequest(PageUtils.pageNumberToIndex(pageable)))
+}

5c6979b1e330580684a003b3e55389241d72ed26
choi-ys <project.log.062@gmail.com>
2022-01-09 21:34:57 +0900

    [#27][주문 API]공통 에러 응답 객체 및 예외 처리 공통 부 정의

---
+++ order-service/src/main/kotlin/io/ecommerce/orderservice/error/ErrorResponse.kt
@@ -0,0 +1,68 @@
+package io.ecommerce.orderservice.error
+
+import com.fasterxml.jackson.annotation.JsonFormat
+import org.springframework.validation.BindingResult
+import java.time.LocalDateTime
+import java.util.*
+import javax.servlet.http.HttpServletRequest
+
+/**
+ * @author : choi-ys
+ * @date : 2022/01/09 9:02 오후
+ */
+data class ErrorResponse(
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss", timezone = "Asia/Seoul")
+    val timestamp: LocalDateTime = LocalDateTime.now(),
+
+    var code: String? = null,
+    var message: String? = null,
+    var method: String? = null,
+    var path: String? = null,
+
+    var errorDetails: List<ErrorDetail>? = null,
+) {
+
+    constructor(
+        errorCode: ErrorCode,
+        httpServletRequest: HttpServletRequest,
+    ) : this() {
+        code = errorCode.code
+        message = errorCode.message
+        method = httpServletRequest.method
+        path = httpServletRequest.requestURI
+        errorDetails = ArrayList()
+    }
+
+
+    constructor(
+        errorCode: ErrorCode,
+        httpServletRequest: HttpServletRequest,
+        bindingResult: BindingResult,
+    ) : this() {
+        this.code = errorCode.code
+        message = errorCode.message
+        method = httpServletRequest.method
+        path = httpServletRequest.requestURI
+        errorDetails = ErrorDetail.of(bindingResult)
+    }
+
+    companion object {
+
+        fun of(
+            errorCode: ErrorCode,
+            httpServletRequest: HttpServletRequest,
+        ): ErrorResponse {
+            return ErrorResponse(errorCode, httpServletRequest)
+        }
+
+        fun of(
+            errorCode: ErrorCode,
+            httpServletRequest: HttpServletRequest,
+            bindingResult: BindingResult,
+        ): ErrorResponse {
+            return ErrorResponse(errorCode, httpServletRequest, bindingResult)
+        }
+    }
+
+}

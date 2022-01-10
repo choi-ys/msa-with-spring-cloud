8678759d343ed3c3f710efd75e4c737f2ab49e2b
choi-ys <project.log.062@gmail.com>
2022-01-10 19:03:13 +0900

    [#29][user-service]로그인 완료 시 JWT 발급을 위한 java-jwt 의존성 추가
     - JWT 발급을 위한 설정 정보 추가(expires term, secret key)

--- user-service/src/main/java/io/ecommerce/userservice/config/security/AuthenticationFilter.java
+++ user-service/src/main/java/io/ecommerce/userservice/config/security/AuthenticationFilter.java
@@ -1,5 +1,7 @@
 package io.ecommerce.userservice.config.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.ecommerce.userservice.core.domain.dto.request.LoginRequest;
 import io.ecommerce.userservice.core.service.UserQueryService;
@@ -12,10 +14,10 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
 
 /**
  * @author : choi-ys
@@ -60,8 +62,26 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
             HttpServletResponse response,
             FilterChain chain,
             Authentication authResult
-    ) throws IOException, ServletException {
+    ) {
         User user = (User) authResult.getPrincipal();
+        response.setHeader("token", tokenBuilder(user));
         // TODO issue JWT Token and set response header
+    }
+
+    private String tokenBuilder(User user) {
+        return JWT.create()
+                .withIssuer("ISSUER")
+                .withSubject("SUBJECT")
+                .withAudience("AUDIENCE")
+                .withIssuedAt(new Date(System.currentTimeMillis()))
+                .withExpiresAt(
+                        new Date(System.currentTimeMillis()
+                                + Long.parseLong(environment.getProperty("token.token-validity-in-seconds-term"))
+                                * 1000)
+                )
+                .withClaim("TYPE", "ACCESS_TOKEN")
+                .withClaim("token", user.getUsername())
+                .sign(Algorithm.HMAC512(environment.getProperty("token.secret-key")))
+                ;
     }
 }

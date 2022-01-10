83ddda23591d6e5597e849e87e567421af0a2085
CHOI-YONG-SEOK <project.log.062@gmail.com>
2022-01-10 14:31:02 +0900

    Revert "[#40]direct forwarding을 위한 RewritePath Filter routes 속성 적용 "

---
+++ product-service/src/main/kotlin/io/ecommerce/productgservice/common/utils/PageUtils.kt
@@ -0,0 +1,17 @@
+package io.ecommerce.productgservice.common.utils
+
+import org.springframework.data.domain.Pageable
+
+/**
+ * @author : choi-ys
+ * @date : 2022/01/09 6:14 오후
+ */
+class PageUtils {
+    companion object {
+        fun pageNumberToIndex(pageable: Pageable): Pageable {
+            return if (pageable.pageNumber != 0)
+                pageable.withPage(pageable.pageNumber - 1)
+            else pageable
+        }
+    }
+}

6843da7256b7ea36b80b97575ca884f1374a5462
choi-ys <project.log.062@gmail.com>
2022-01-13 20:29:03 +0900

    [#57][product-service]주문 Event 수신 시 상품 재고 감소 처리
    - Event 구독을 위한 Kafka 의존성 및 Consumer 설정 추가
    - 주문 발생 Event 수신 및 상품 재고 처리
    - Kafka 관련 docker-compose 작성

---
+++ product-service/src/main/kotlin/io/ecommerce/productgservice/config/kafka/KafkaConsumerConfig.kt
@@ -0,0 +1,43 @@
+package io.ecommerce.productgservice.config.kafka
+
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.annotation.EnableKafka
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.core.ConsumerFactory
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+import java.util.*
+
+/**
+ * @author : choi-ys
+ * @date : 2022/01/13 8:25 오후
+ */
+@EnableKafka
+@Configuration
+class KafkaConsumerConfig {
+    @Value("\${spring.kafka.bootstrap-servers}")
+    private val bootstrapServers: String? = null
+
+    @Value("\${spring.kafka.consumer.group-id}")
+    private val groupId: String? = null
+
+    @Bean
+    fun consumerFactory(): ConsumerFactory<String?, String?> {
+        val configProps: MutableMap<String, Any> = HashMap()
+        configProps[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = bootstrapServers!!
+        configProps[ConsumerConfig.GROUP_ID_CONFIG] = groupId!!
+        configProps[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
+        configProps[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
+        return DefaultKafkaConsumerFactory(configProps)
+    }
+
+    @Bean
+    fun kafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, String>? {
+        val kafkaListenerContainerFactory = ConcurrentKafkaListenerContainerFactory<String, String>()
+        kafkaListenerContainerFactory.consumerFactory = consumerFactory()
+        return kafkaListenerContainerFactory
+    }
+}

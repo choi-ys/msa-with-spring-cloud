3b5ef806c7287d2baeca22e1d483e25b1a826c7d
choi-ys <project.log.062@gmail.com>
2022-01-13 20:24:43 +0900

    [#57][order-service]Kafak를 이용한 주문 Event 발생 시 상품 재고 처리
     - Event 발행을 위한 Kafka 의존성 및 Producer 설정 추가
     - 주문 발생 시 상품 재고 처리를 위한 Event 발행

---
+++ order-service/src/main/kotlin/io/ecommerce/orderservice/config/kafka/KafkaProducerConfig.kt
@@ -0,0 +1,35 @@
+package io.ecommerce.orderservice.config.kafka
+
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.core.ProducerFactory
+
+/**
+ * @author : choi-ys
+ * @date : 2022/01/13 2:05 오후
+ */
+@Configuration
+class KafkaProducerConfig {
+
+    @Value("\${spring.kafka.bootstrap-servers}")
+    private val bootstrapServers: String? = null
+
+    @Bean
+    fun producerFactory(): ProducerFactory<String, String> {
+        val configProps = HashMap<String, Any>()
+        configProps[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = bootstrapServers!!
+        configProps[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+        configProps[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+        return DefaultKafkaProducerFactory(configProps)
+    }
+
+    @Bean
+    fun kafkaTemplate(): KafkaTemplate<String, String> {
+        return KafkaTemplate(producerFactory())
+    }
+}
